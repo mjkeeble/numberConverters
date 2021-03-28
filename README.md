0b6fbbefb0b59706f40d5e579c8fb980b32d4c46
Mark Keeble <mjkeeble@yahoo.de>
2021-03-28 20:59:31 +0200

    chore: initial commit of react app README added numbers2text.js added from IronSnippets

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# numberConverters
+# Number Converters
+
+This repo now contains the numbersToText file previously included in the [IronSnippets](https://github.com/mjkeeble/IronSnippets). My intention is to expand this into a stand-alone web site and API with various number conversion functions
+* numbers to text conversion
+  * English
+  * German
+  * French
+  * contributions for other languages welcome!
+* text to numbers conversion (maybe!)
+* numbers to roman numerals (and back)
+* base conversions
+
+# numberToWords
+(numberToWords.js)
+Set of functions to convert integers, decimals and currencies to words.
+This may not be the most elegant approach to converting numbers, so improvement suggestions are welcome!
+
+### float2Words
+Returns the amount entered as a text string in words.
+
+Parameters:
+1. num - the number to be converted.
+
+*The code expects a decimal point.* If a comma is used replace the '.' in the split method in line 7 with a comma.
+
+*Dependencies:* Calls integer2Words and hundreds2Words functions.
+
+### currency2Words
+Returns a monetary value as a text string in words. 
+
+Parameters:
+1. num - the value to be converted
+1. 'currency' (optional) - three-letter currency code
+
+The currency list includes dollar, euro and pounds - additional currencies can be added to the currency object after line 23.
+
+Uses the US numbering system for billions.
+
+*Dependencies:* Calls integer2Words and hundreds2Words functions. 
+
+### integer2Words
+Returns an integer as a text string in words.
+
+Parameters:
+1. num - the value to be converted
+1. 'sys' (optional) - enter 'us' to use the American numbering system where 10^9 = 1 billion, otherwise 10^12 = 1 billion
+
+*Dependencies:* calls hundreds2Words function.
+
+### hundreds2Words
+Returns an integer value below 1000 as a text string in words.
+
+Can be used on a stand-alone basis, but is concieved as a subordinated function to the others.
+
+This function can be used on a stand-alone basis where appropriate, but was concieved as a subordinate function to integer2Words.
+
---
+++ numbersToWords.js
@@ -0,0 +1,105 @@
+// Please refer to the IronSnippets ReadMe for information on these functions
+// This file is part of IronSnippets by Mark J. Keeble
+// https://github.com/mjkeeble/IronSnippets
+
+function float2Words(num) {
+  let floatInText = "";
+  let decimal = num.toString().split(".")[1];
+  if (Math.floor(num) == 0) {
+      floatInText = "Zero point"
+  } else {
+      floatInText = integer2Words(Math.floor(num)) + " point";
+  } // End if
+  console.log(decimal);
+  for (let i = 0; i < decimal.length; i++) {
+      floatInText += " " + integer2Words(decimal.charAt(i));
+  } // end for loop
+  return floatInText.trim();
+} //End float2Words
+
+function currency2Words(value, currency) {
+  let currencyInText = integer2Words(Math.floor(value), "us");
+  const currencies = {
+      //["Currency code", "currency unit", "subdivision plural", "subdivision singular"]
+      "USD": ["dollar", "cents", "cent"],
+      "EUR": ["euros", "cents", "cent"],
+      "GBP": ["pounds", "pence", "pence"]
+  };
+  console.log(currency);
+  console.log(Math.floor((value % 1) * 100));
+  
+
+  let unit = currencies[currency.toUpperCase()][0];
+  let subunit = Math.floor((value % 1) * 100) < 2 ? currencies[currency.toUpperCase()][2] : currencies[currency.toUpperCase()][1];
+  if (Math.floor(value) >= 1) currencyInText += " " + unit
+  if (Math.floor(value) != value) {
+      if (Math.floor(value) > 1) currencyInText += " and "
+      currencyInText += integer2Words(Math.floor((value % 1) * 100))
+          + " "
+          + subunit;
+  } // end if
+  return (currencyInText.charAt(0).toUpperCase() + currencyInText.slice(1)).trim()
+} // End currency2Words
+
+function integer2Words(num, sys = "") {
+  let numInText = ""
+  console.log(num);
+  
+  num = parseInt(num);
+  const thousands = ["", "thousand", "million", "thousand", "billion", "thousand", "trillion", "quadrillion", "quintillion", "sextillion"];
+
+  if (sys.toLowerCase() == "us") {
+      thousands.splice(5, 1);
+      thousands.splice(3, 1);
+  } // end if
+  num = num.toString();
+  let i = 1;
+  while (i < (num.length / 3)) {
+      numInText = (hundreds2Words(num.substring(num.length - (i * 3), num.length - (i - 1) * 3)))
+          + " "
+          + thousands[i - 1]
+          + " "
+          + numInText;
+      i++
+  } //end while
+  numInText = (hundreds2Words((num.substring(0, num.length - ((i - 1) * 3)))))
+      + " "
+      + thousands[i - 1]
+      + " "
+      + numInText;
+  console.log(numInText);
+
+  return (numInText.charAt(0).toUpperCase() + numInText.slice(1)).trim();
+
+} // End integer2Words
+
+function hundreds2Words(num) {
+  const zero2Nineteen = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+  let txt = "";
+  if (num.length == 3) {
+      txt += zero2Nineteen[num.charAt(0)] + " hundred";
+      num = num.substring(1)
+      if (num > 0) {
+          txt += " and"
+      } else {
+          return txt;
+      } // End if
+  } // End if
+  if (num < 20) {
+      txt += " " + zero2Nineteen[parseInt(num)];
+  } else {
+      txt += " " + tens[Math.floor(parseInt(num) / 10)];
+      if (parseInt(num) % 10 > 0) txt += "-";
+      txt += zero2Nineteen[Math.floor(parseInt(num) % 10)];
+  } // End if
+  return txt;
+} // End hundreds2Words
+
+
+// tests
+console.log(currency2Words(65.02, "eur"))
+console.log(integer2Words(73628483723));
+array.forEach(element => {
+  
+});
